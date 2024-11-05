a916305c32309b547b2b98695eae4ab7f04e06ff
dakshrao98 <dakshrao.1998@gmail.com>
2024-11-05 10:22:42 +0000

    test

---
+++ .devcontainer/01_basics/datesInJs.js
@@ -0,0 +1,14 @@
+// dates
+
+// let myDate = new Date() 
+// console.log(myDate)
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleTimeString())
+// let myCreatedDate = new Date (2024 , 10 , 12)
+// console.log(myCreatedDate.toString())
+// let myCreatedDate1 = new Date (2024 , 10 , 12 , 11 ,13 ,13 )
+// console.log(myCreatedDate1.toString())
+// let myTimeStamp = Date.now();
+//  console.log(myTimeStamp)
+console.log(Math.floor(Date.now()/1000)) //converted from millisec to sec
+
---
+++ .devcontainer/01_basics/numbersAndMath.js
@@ -0,0 +1,27 @@
+// const score = 33
+// const balance = new Number(33.3224413413434)
+// console.log(balance)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(3))
+// const number = 342.63441
+// console.log(number.toPrecision(3))
+// const number2 = 10200002
+// console.log(number2.toLocaleString('en-IN'))
+
+
+
+/////////++++++++++++++++++++++
+// maths
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.1)) //round up 
+// console.log(Math.floor(4.9)) //round down
+// console.log(Math.max(4.6 , 4.4 , 4.7))
+// console.log(Math.min(4.6 , 4.4 , 4.7))
+// console.log(Math.random())
+// console.log(Math.floor((Math.random()*10))+1)
+const max = 20
+const min = 10
+
+console.log(Math.floor((Math.random())* (max - min +1) + min))
